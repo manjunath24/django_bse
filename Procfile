0da42871f5b9bd5034b5bd6492b3f9a4a58f1f4b
manjunath24 <hugar.manjunathrt@gmail.com>
2014-07-30 14:10:34 +0530

    Moving to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+eb: gunicorn django_bse.wsgi --log-file -
--- django_bse/settings.py
+++ django_bse/settings.py
@@ -85,3 +85,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
